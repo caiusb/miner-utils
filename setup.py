3e43083f6f6991aac4813815cd3d5ff0038ba12d
Caius Brindescu <brindesc@oregonstate.edu>
2017-05-02 13:41:53 -0700

    setup.py and __init__.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='GitHubUtils',
+      version='1.0',
+      description='GitHub API Access Utilities',
+      author='Caius Brindescu',
+      author_email='caius@brindescu.com',
+      url='https://github.com/caiusb/github-utils',
+      packages=['requests'],
+     )
