1885fe71389ab7b6bbaeb17477e2a3a217d1da07
Caius Brindescu <caius.brindescu@gmail.com>
2019-01-10 16:30:01 -0800

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='MinerUtils',
-      version='2.0.3',
+      version='2.0.4',
       description='GitHub and Travis API Access Utilities',
       author='Caius Brindescu',
       author_email='caius@brindescu.com',
